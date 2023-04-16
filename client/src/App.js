0411322d9ce8e043ca9b8112224af41e18c41cd0
jcarranza06 <72419038+jcarranza06@users.noreply.github.com>
2023-04-16 18:35:31 -0500

    se crea primera version de Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
