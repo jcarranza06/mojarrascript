feecd3c2b379c6d8636cdaa823077ebd085118d2
JuanMahecha <jmahechat@unal.edu.co>
2023-05-20 12:18:07 -0500

    Se creo el footer y la pantalla donde se ven las listas de compras

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import Home from './components/Home';
+import{List,NewLista} from './components/ListasCompras';
 import Navbar from './components/Navbar';
-
+import Footer from './components/footer';
+//<List/>
+//<NewLista/>
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Home/>
+      <Footer/> 
     </div>
   );
 }
