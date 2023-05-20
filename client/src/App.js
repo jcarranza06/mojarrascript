1b0b7d758280c44a328c72e0a2af48d036f16e98
Martin Polanco <mpolancob@unal.edu.co>
2023-05-20 05:16:30 -0500

    Agregue resultado de busqueda y mod perfil usuario

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import PreProducto from './components/PreProducto';
+import Perfil from './components/Perfil';
+
 
 function App() {
   return (
