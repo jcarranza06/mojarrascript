82c81db3dcaa08a735167d6e9b3b8c3cf76c4c3c
jcarranza <72419038+jcarranza06@users.noreply.github.com>
2023-05-22 11:34:06 -0500

    se agrega lo compoente de mapa de cristian y además se agregan metodos para mostrar ubicaion actual del usuario en Home

---
+++ client/src/components/MapContainer.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const MapComponent = () => {
+  useEffect(() => {
+    // Crea una instancia del loader
+    const loader = new Loader({
+      apiKey: 'AIzaSyC8M5k9Y7wSaUmjoW4ySE3Z4NYKW9DQXfA',
+      version: 'weekly',
+    });
+
+    // Carga la API de Google Maps
+    loader.load().then(() => {
+      // Una vez que la API se ha cargado correctamente se inicializa el mapa
+      //initMap();
+      getUserPos();
+    });
+  }, []);
+
+  const opcionesDeSolicitud = {
+    enableHighAccuracy: true, // Alta precisión
+    maximumAge: 0, // No queremos caché
+    timeout: 5000 // Esperar solo 5 segundos
+};
+
+const onErrorDeUbicacion = err => {
+    console.log("Error obteniendo ubicación: ", err);
+}
+
+  const getUserPos = () =>{
+    navigator.geolocation.getCurrentPosition(initMap, onErrorDeUbicacion, opcionesDeSolicitud);
+  };
+
+  // Función para inicializar el mapa
+  const initMap = location => {
+    console.log(location.coords.latitude);
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: location.coords.latitude, lng: location.coords.longitude },
+      zoom: 15,
+    });
+  };
+
+  return <div id="map" style={{ width: '100%', height: '400px' }}></div>;
+};
+
+export default MapComponent;
